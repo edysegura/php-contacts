a05dbe7da0b1f166dc23a7b0449c9006de57bc7c
Edy Segura <edysegura@gmail.com>
2020-01-03 15:31:41 -0300

    Update insert.sql

--- src/database/insert.sql
+++ src/database/insert.sql
@@ -1,2 +1,2 @@
 INSERT INTO contato (nome, email, telefone, celular, foto, descricao) VALUES 
-('Daniel Santos', 'daniel.si@gmail.com', '+55 35 3449-2168', '+55 35 8805-2210', '', '');
+('Davi Bernardes Segura', 'davisegura@gmail.com', '+55 35 3449-2168', '+55 35 8805-2210', '', '');
